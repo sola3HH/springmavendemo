3ed7ffba42fd05ce4482049730f48ba9662d4529
sanhuanghuang <13551296510@163.com>
2020-09-09 17:22:46 +0800

    fix: resttemplate添加了中文支持

--- src/main/java/com/hh/springmavendemo/config/RestTemplateConfig.java
+++ src/main/java/com/hh/springmavendemo/config/RestTemplateConfig.java
@@ -4,7 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author YAOSHUNYU
@@ -17,7 +20,9 @@ public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate(ClientHttpRequestFactory factory) {
-        return new RestTemplate(factory);
+        RestTemplate restTemplate = new RestTemplate(factory);
+        restTemplate.getMessageConverters().set(1, new StringHttpMessageConverter(StandardCharsets.UTF_8));
+        return restTemplate;
     }
 
     @Bean
